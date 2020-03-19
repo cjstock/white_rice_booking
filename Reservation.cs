b40591cd19d4150baac0eb18bb2659e0645bb68b
TimothyBui <50809539+TimothyBui@users.noreply.github.com>
2020-03-19 14:52:36 -0700

    outline what "reservation" holds

--- Reservation.cs
+++ Reservation.cs
@@ -4,6 +4,15 @@ namespace white_rice_booking
 {
     public class Reservation
     {
-         
+        //reservation id?
+
+        //departing airport ID
+        //departure time
+
+        //destination airport ID
+        //arrival time
+
+        //date
+
     }
 }
