3e3c8545e0df621b8e96bcbce67c22f734c22dc0
Corey <coreystock@gmail.com>
2020-03-26 13:17:02 -0700

    Added ID to Entity

--- UserAccount.cs
+++ UserAccount.cs
@@ -5,6 +5,7 @@ namespace white_rice_booking
 {
     public class UserAccount
     {
+        public int ID { get; set; }
         public string Email { get; set; }
 
         public string Password { get; set; }
