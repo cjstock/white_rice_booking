b3b539af70761043e951a034615855dc5de5d259
yungwill <williamyung777@gmail.com>
2020-04-08 10:17:40 -0700

    Able to access database to check airports

--- Controllers/FilterFlightsController.cs
+++ Controllers/FilterFlightsController.cs
@@ -1,3 +1,15 @@
+/*
+    Name: Filter Flights Controller
+    Date Last Updated: 4-08-2020
+    Programmer Names: Corey Stock, William Yung
+    Description: This class is the controller that will control how the website will filter the different
+                 flights for the user to view and choose from
+    Important Methods/Structures/Etc: 
+            Functions - FilterFlights, FilterOutgoing, FilterIncoming
+    Major Decisions: Use built in search function to look for data in the database because it keeps the code 
+                     cleaner and easier to use
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,6 +17,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using CsvHelper;
+using System.IO;
+using System.Globalization;
 
 namespace white_rice_booking
 {
@@ -30,16 +45,24 @@ namespace white_rice_booking
         public DateTime incomingDepart { get; set; }
         public DateTime incomingArrival { get; set; }
 
-
         public FilterFlightsController(ILogger<FilterFlightsController> logger)
         {
             _logger = logger;
         }
 
+
+        /*
+            Name: Filter Flights
+            Date Last Updated: 4-08-2020
+            Last Updated Programmer Name: William Yung
+            Description: This function opens the database and checks if the user wants a round trip or one way flight
+        */
         [HttpGet]
         [Route("filter/{trip_type}/{depart_loc}")]
         public String FilterFlights(string trip_type, string depart_loc)
         {   
+            
+           
 
             if (trip_type == "O") twoway = false;
             else if (trip_type == "T") twoway = true;
@@ -55,17 +78,27 @@ namespace white_rice_booking
         [Route("out_filter/{depart_loc}")]
         public String FilterOutgoing(string depart_loc)
         {
-             if (twoway)
-             {
-                 //return FilterIncoming();
-                 //return twoway;
-                 if(depart_loc == "LAX") return "Los Angeles International Airport";
-                 else return "No Airport Selected for round trip";  
-             }
-             else return "No Airport Selected for one way trip";
+            using (var airport_reader = new StreamReader("database\\top_10_airports.csv"))
+            using (var csv = new CsvReader(airport_reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<Airport>();
+                if (twoway)
+                {
+                    //return FilterIncoming();
+                    //return twoway;
+                    foreach (var record in records)
+                    {
+                        
+                        if(depart_loc == record.IATA) return "Los Angeles International Airport";
+                        //else return "No Airport Selected for round trip"; 
+                        
+                    }
+                    
+                }
+                else return "No Airport Selected for one way trip";
+            }
+            return "Holder";
     
-             
-
         }
 
         /* Defaults to reverse of Outgoing airports */
---
+++ Models/Airport.cs
@@ -0,0 +1,10 @@
+public class Airport
+{
+    //[Name("OpenFlights_ID")]
+    public int OpenFlights_ID { get; set; }
+    public string Airport_Name { get; set; }
+    public string City_Name { get; set; }
+    public string IATA { get; set; }
+    public int Timezone { get; set; }
+    public string Timezone_Name { get; set; } 
+}
