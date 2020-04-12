5b6098b780cc2cbfdc037bd460cb0a142494aad2
Justin Tran <justin.s.tran97@gmail.com>
2020-04-11 22:09:01 -0700

    Added code to allow use of .css and .js files with project

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,12 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+//using Microsoft.Extensions.Logging;
+
+
 
 namespace white_rice_booking.Pages
 {
@@ -15,3 +21,23 @@ namespace white_rice_booking.Pages
         }
     }
 }
+
+/*
+namespace white_rice_booking.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
+*/
--- Startup.cs
+++ Startup.cs
@@ -14,12 +14,14 @@ namespace white_rice_booking
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -36,6 +38,8 @@ namespace white_rice_booking
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles(); // Allows HTML, CSS, images, and JavaScript files in wwwroot 
+                                  // to be used
             app.UseRouting();
 
             app.UseAuthorization();
