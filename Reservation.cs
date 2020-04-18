ed6cebf2e3cc60810493191ac8de6995ac2b4eb7
TimothyBui <50809539+TimothyBui@users.noreply.github.com>
2020-04-18 15:06:52 -0700

    updated Reservation.cs

--- Reservation.cs
+++ Reservation.cs
@@ -5,12 +5,11 @@ namespace white_rice_booking
 {
     public class Reservation
     {
-        public int ID { get; set; }
         public string PassengerFN { get; set; }
         public string PassengerLN { get; set; }
-        public int OutboundFlightID { get; set; }
-        public int InboundFlightID { get; set; }
+        public int DepartFlightID { get; set; }
+        public int ReturnFlightID { get; set; }
         public int UserAccountID { get; set; }
-
+        public int ReservationID { get; set; }
     }
 }
