8d1173eb76acd9a7eb7e891ed48f7939fd50da54
TimothyBui <50809539+TimothyBui@users.noreply.github.com>
2020-04-29 13:37:57 -0700

    fixed display issues

--- Pages/ReservationConfirm.cshtml.cs
+++ Pages/ReservationConfirm.cshtml.cs
@@ -47,22 +47,63 @@ namespace white_rice_booking.Pages
         [BindProperty(SupportsGet = true)]
         public int ZipCode{ get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public int route_id{ get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int depart_flight_id{ get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int arrival_id{ get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string depart_airport_name{ get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string depart_city_name{ get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string arrival_airport_name{ get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string arrival_city_name{ get; set; }
+
         public void OnGet()
-        {
-            //display everything
-            int flightID = 0;
-            
-            //seats, date, time, depart airport, depart city, arrival airport, arrival city, billing
+        { 
+            //todo: seats, date, time, depart airport, depart city, arrival airport, arrival city, billing
 
-
+            //calling flight information
+            /*int flightID = 539567;
             string temp = _reservationService.FindFlightInfo(flightID);
             string[] words = temp.Split(','); //string array output 
+
+            //Parsing the output string array back into variables
+            route_id = Int32.Parse(words[0]);
+            depart_flight_id = Int32.Parse(words[1]);
+            arrival_id = Int32.Parse(words[2]);
+            depart_airport_name = words[3];
+            depart_city_name = words[4];
+            arrival_airport_name = words[5];
+            arrival_city_name = words[6];*/
         }
 
         public IActionResult OnPost()
         {
             int userID = 159753;
             _reservationService.Create(FirstName, LastName, depart_ID, return_ID, userID);
+            //calling flight information
+            int flightID = 539567;
+            string temp = _reservationService.FindFlightInfo(flightID);
+            string[] words = temp.Split(','); //string array output 
+
+            //Parsing the output string array back into variables
+            route_id = Int32.Parse(words[0]);
+            depart_flight_id = Int32.Parse(words[1]);
+            arrival_id = Int32.Parse(words[2]);
+            depart_airport_name = words[3];
+            depart_city_name = words[4];
+            arrival_airport_name = words[5];
+            arrival_city_name = words[6];
             return Page();
         }
     }
